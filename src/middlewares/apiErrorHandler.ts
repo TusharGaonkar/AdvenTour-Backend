2061912de9a3e6f8fb7c154611e65ca28d844345
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2023-11-03 18:22:40 +0530

    main error handler middleware for our app , handles both client and server side errors and sends appropriate response to the client

---
+++ src/middlewares/apiErrorHandler.ts
@@ -0,0 +1,43 @@
+import { Request, Response, NextFunction } from 'express';
+import AdentourAppError from '../utils/adventourAppError';
+
+const handleProductionErrors = (error: AdentourAppError, res: Response) => {
+  // send only the client side errors as a response
+  if (error.isClientError)
+    res.status(error.statusCode).json({
+      status: error.status,
+      message: error.message,
+    });
+  else
+    console.log(
+      "Something went wrong in the AdvenTour Server it's not an error from your end , please contact gaonkar.tushar01@gmail.com!"
+    );
+};
+
+const handleDevelopmentErrors = (error: AdentourAppError, res: Response) => {
+  res.status(error.statusCode).json({
+    status: error.status,
+    message: error.message,
+    stack: error.stackTrace,
+    errorDetail: error,
+  });
+};
+const apiErrorHandler = (
+  error: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const serverError = new AdentourAppError(
+    error.message ?? 'Something went wrong',
+    //throw generic server fail error code if not present!
+    error.statusCode ?? 500
+  );
+  if (process.env.NODE_ENV === 'production') {
+    handleProductionErrors(serverError, res);
+  } else if (process.env.NODE_ENV == 'development') {
+    handleDevelopmentErrors(serverError, res);
+  }
+};
+
+export default apiErrorHandler;
