6437b6eafbe09e965d4bfdf39c596a089f38d508
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2023-11-03 18:12:13 +0530

    custom error handler middleware configured

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
-import express from 'express';
-import morgan from 'morgan';
-import addRequestTime from './middlewares/addRequestTime';
-import tourRouter from './routers/tourRouter';
-import path from 'path';
 import dotenv from 'dotenv';
-
-const app = express();
+import path from 'path';
 
 dotenv.config({
   path: path.join(__dirname, '..', 'config.env'),
 });
+
+import express from 'express';
+import morgan from 'morgan';
+import addRequestTime from './middlewares/addRequestTime';
+import tourRouter from './routers/tourRouter';
+import AdentourAppError from './utils/adventourAppError';
+import apiErrorHandler from './middlewares/apiErrorHandler';
+
+const app = express();
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -21,5 +24,17 @@ app.use(express.json());
 app.use(addRequestTime);
 
 app.use('/api/v-1.0/tours', tourRouter);
+
+app.use('*', (req, res, next) => {
+  next(
+    new AdentourAppError(
+      `No route found for ${req.originalUrl} on this server!`,
+      404
+    )
+  );
+});
+
+// Custom error handler middleware
+app.use(apiErrorHandler);
 
 export default app;
