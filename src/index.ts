119a540e8d65e46a7234a18369c86e51c23a9809
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2023-11-08 17:56:12 +0530

    authentication configured in the middleware

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,14 @@ dotenv.config({
   path: path.join(__dirname, '..', 'config.env'),
 });
 
+import cookieParser from 'cookie-parser';
 import express from 'express';
 import morgan from 'morgan';
 import addRequestTime from './middlewares/addRequestTime';
 import tourRouter from './routers/tourRouter';
 import AdentourAppError from './utils/adventourAppError';
 import apiErrorHandler from './middlewares/apiErrorHandler';
+import passport from './authentication/passport'; // just import the passport conf , strategies already configured
 
 const app = express();
 
@@ -20,11 +22,16 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('combined'));
 }
 
+app.use(cookieParser()); // need to parse cookies in req.cookies
 app.use(express.json());
 app.use(addRequestTime);
 
 app.use('/api/v-1.0/tours', tourRouter);
-
+app.post(
+  '/api/v-1.0/login',
+  passport.authenticate('jwt', { session: false, failWithError: true }),
+  (req, res) => console.log(req)
+);
 app.use('*', (req, res, next) => {
   next(
     new AdentourAppError(
