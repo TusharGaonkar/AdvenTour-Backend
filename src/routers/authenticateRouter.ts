180ee57e852fd15b46cfe129243aedf760df9031
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2023-11-10 20:08:04 +0530

    route handler for "api/v-1.0/auth" requests

---
+++ src/routers/authenticateRouter.ts
@@ -0,0 +1,48 @@
+import express from 'express';
+import passport from '../authentication/passport';
+import AdventourAppError from '../utils/adventourAppError';
+import { HydratedDocument } from 'mongoose';
+import loginUser from '../controllers/loginUserController';
+import registerNewUser from '../controllers/registerNewUserController';
+
+const authenticateRouter = express.Router();
+
+authenticateRouter.route('/login').post(loginUser);
+
+authenticateRouter.route('/register').post(registerNewUser);
+
+authenticateRouter.route('/google').get(
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+    session: false,
+  })
+);
+
+authenticateRouter.route('/google/callback').get(
+  passport.authenticate('google', {
+    failureRedirect: '/auth/google/failure',
+    session: false,
+  }),
+  (req, res) => {
+    const user: HydratedDocument<any> = req.user;
+    const token = user.generateJwtToken();
+
+    res.cookie(process.env.JWT_TOKEN_KEY, token, {
+      httpOnly: true,
+      secure: true, // can't be read in the client side javascript ; prevent cross-site scripting and token stealing!
+    });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        user,
+        token,
+      },
+    });
+  }
+);
+
+authenticateRouter.route('/google/failure').get((req, res, next) => {
+  next(new AdventourAppError('Google Authentication Failed', 401));
+});
+
+export default authenticateRouter;
