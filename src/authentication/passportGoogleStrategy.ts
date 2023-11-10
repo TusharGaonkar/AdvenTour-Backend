9233ba246128c75c110851568ac317a4addff823
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2023-11-10 18:41:36 +0530

    google o-auth integrated , not using sessions using custom jwt token with this

--- src/authentication/passportGoogleStrategy.ts
+++ src/authentication/passportGoogleStrategy.ts
@@ -1,17 +1,40 @@
 import { Strategy, StrategyOptions } from 'passport-google-oauth20';
 import passport from 'passport';
+import Users from '../models/userModel';
 
 const options: StrategyOptions = {
-  clientID: 'ijjdijfdif',
-  clientSecret: 'dfjdfijdfijdif',
-  callbackURL: 'ijfidjfidjfijfidfj',
+  clientID:
+    '705828693897-o5tpmrf0prle2qqk9mmea4h0c9fm3jvh.apps.googleusercontent.com',
+  clientSecret: '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',
+  callbackURL: 'http://localhost:2000/api/v-1.0/auth/google/callback',
 };
 
-const googleStrategy = new Strategy(
-  options,
-  (_accessToken, _refreshToken, profile, done) => {
-    return done(null, profile);
+const googleStrategy = new Strategy(options, async function (
+  _accessToken,
+  _refreshToken,
+  profile,
+  cb
+) {
+  const { name: userName, email, picture: avatar } = profile._json;
+
+  try {
+    const existingUser = await Users.findOne({ email, authProvider: 'google' });
+
+    if (existingUser) return cb(null, existingUser);
+
+    const newUser = await Users.create({
+      userName,
+      email,
+      avatar,
+      authProvider: 'google',
+    });
+
+    return cb(null, newUser);
+  } catch (error) {
+    return cb(error);
   }
-);
+});
+
+passport.use(googleStrategy);
 
 export default googleStrategy;
