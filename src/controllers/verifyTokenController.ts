7df910fc170e23be1979eee3fc4c69ad7d6b4c89
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2023-12-02 11:26:36 +0530

    re-authentication added ,  must be used while granting access to secure resources

---
+++ src/controllers/verifyTokenController.ts
@@ -0,0 +1,30 @@
+import { Request, Response, NextFunction } from 'express';
+import passport from 'passport';
+export const verifyToken = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  return passport.authenticate('jwt', { session: false, failWithError: true })(
+    req,
+    res,
+    next
+  );
+};
+
+export const successResponse = (
+  req: Request & { user: any },
+  res: Response
+) => {
+  const { userName, email, role } = req.user;
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: {
+        userName,
+        email,
+        role,
+      },
+    },
+  });
+};
