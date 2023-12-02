017013a8db6b6476a2fcbfadbe47b7152cfec903
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2023-12-02 11:28:45 +0530

    autocomplete endpoint added

--- src/routers/tourRouter.ts
+++ src/routers/tourRouter.ts
@@ -10,18 +10,19 @@ import express from 'express';
 
 import requiresAuthentication from '../middlewares/authenticationHandler';
 import requiresAuthorization from '../middlewares/authorizationHandler';
+import searchSuggestionsController from '../controllers/searchSuggestionsController';
 
 const tourRouter = express.Router();
 
 // GET is public , posting required authentication!
-tourRouter
-  .route('/')
-  .get(getAllTours)
-  .post(
-    requiresAuthentication,
-    requiresAuthorization(['user', 'admin']),
-    createSingleTour
-  );
+tourRouter.route('/').get(getAllTours).post(
+  // requiresAuthentication,
+  // requiresAuthorization(['user', 'admin']),
+  createSingleTour
+);
+
+// search autocomplete route!
+tourRouter.route('/suggestions').get(searchSuggestionsController);
 
 tourRouter
   .route('/:tourID')
