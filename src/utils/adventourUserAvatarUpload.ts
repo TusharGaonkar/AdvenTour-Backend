028dbcba3d8b8e0c3d0f58d460287e0fa40de823
Tushar <gaonkar.tushar01@gmail.com>
2024-03-19 13:36:20 +0530

    feat: multer configuration for saving tour review images locally

---
+++ src/utils/adventourUserAvatarUpload.ts
@@ -0,0 +1,55 @@
+import multer from 'multer';
+import crypto from 'crypto';
+import path from 'path';
+import express from 'express';
+import fs from 'fs';
+
+const storage = multer.diskStorage({
+  filename: (req, file, cb) => {
+    const uniqueFileName = crypto.randomBytes(8).toString('hex');
+    const fileExtension = path.extname(file.originalname);
+    cb(null, uniqueFileName + fileExtension);
+  },
+
+  destination: (req, file, cb) => {
+    const reviewImagesUploadDir = path.join(
+      __dirname,
+      '..',
+      '..',
+      'public',
+      'user-avatar-upload'
+    );
+
+    try {
+      if (!fs.existsSync(reviewImagesUploadDir)) {
+        fs.mkdirSync(reviewImagesUploadDir, { recursive: true });
+      }
+    } catch (err) {
+      cb(err, reviewImagesUploadDir);
+    }
+
+    cb(null, reviewImagesUploadDir);
+  },
+});
+
+const fileFilter = (
+  req: express.Request,
+  file: Express.Multer.File,
+  cb: multer.FileFilterCallback
+) => {
+  const { mimetype } = file;
+
+  if (mimetype && mimetype.includes('image')) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const updateUserAvatar = multer({
+  storage,
+  fileFilter,
+  limits: { fileSize: 3 * 1024 * 1024 },
+});
+
+export default updateUserAvatar;
